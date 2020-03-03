9c7eae91b75049874516916a02f80662ddc8ab2d
Justin <justt0023@gmail.com>
2020-03-02 21:06:10 -0600

    We put the rotation count on the dashboard.

--- src/main/java/team/gif/robot/subsystems/ColorSensor.java
+++ src/main/java/team/gif/robot/subsystems/ColorSensor.java
@@ -89,6 +89,9 @@ public class ColorSensor extends SubsystemBase {
     System.out.println("Color Senor Initiated");
   }
 
+  public int rotationCount = 0;
+  public String previousColor = "";
+  String[] colorOrder = {"yellow", "blue", "green", "red"};
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
@@ -130,76 +133,34 @@ public class ColorSensor extends SubsystemBase {
      * Open Smart Dashboard or Shuffleboard to see the color detected by the
      * sensor.
      */
-//    SmartDashboard.putNumber("Red", detectedColor.red);
-//    SmartDashboard.putNumber("Green", detectedColor.green);
-//    SmartDashboard.putNumber("Blue", detectedColor.blue);
-//    SmartDashboard.putNumber("Confidence", match.confidence);
-//    SmartDashboard.putString("Detected Color", colorString);
-    /**
-     * The method GetColor() returns a normalized color value from the sensor and can be
-     * useful if outputting the color to an RGB LED or similar. To
-     * read the raw color, use GetRawColor().
-     *
-     * The color sensor works best when within a few inches from an object in
-     * well lit conditions (the built in LED is a big help here!). The farther
-     * an object is the more light from the surroundings will bleed into the
-     * measurements and make it difficult to accurately determine its color.
-     */
-//    System.out.println("In robot periodic");
-//    Color detectedColor = m_colorSensor.getColor();
-
-    /**
-     * The sensor returns a raw IR value of the infrared light detected.
-     */
-//    double IR = m_colorSensor.getIR();
-
-    /**
-     * Open Smart Dashboard or Shuffleboard to see the color detected by the
-     * sensor.
-     */
     SmartDashboard.putNumber("Red", detectedColor.red);
     SmartDashboard.putNumber("Green", detectedColor.green);
     SmartDashboard.putNumber("Blue", detectedColor.blue);
+
+
 //    SmartDashboard.putNumber("IR", IR);
-
     /**
-     * In addition to RGB IR values, the color sensor can also return an
-     * infrared proximity value. The chip contains an IR led which will emit
-     * IR pulses and measure the intensity of the return. When an object is
-     * close the value of the proximity will be large (max 2047 with default
-     * settings) and will approach zero when the object is far away.
-     *
-     * Proximity can be used to roughly approximate the distance of an object
-     * or provide a threshold for when an object is close enough to provide
-     * accurate color values.
+     * Attempting to count rotations
      */
-//    int proximity = m_colorSensor.getProximity();
-//
-//    SmartDashboard.putNumber("Proximity", proximity);
-
-    /**
-     * Dispays our guess of the color.
-     * Order is confidence level.
-     * All are with sensor light on.
-     * 1: Red - Highest value = Red
-     * 2: Yellow - Highest value = Green, R > B
-     * 3: Green - Highest value = Green, B > R, Blue and Red within 10
-     * 4: Blue - Green & Blue > Red, G & B are within 5, Blue and Red greater than 10
-     */
-//    String colorGuess = "";
-//    double redBlueDifference = Math.abs(detectedColor.blue - detectedColor.red);
-//    if(detectedColor.red > detectedColor.blue && detectedColor.red > detectedColor.green) {
-//      colorGuess = "Red";
-//
-//    } else if(detectedColor.green > detectedColor.red && detectedColor.green > detectedColor.blue && detectedColor.red > detectedColor.blue) {
-//      colorGuess = "Yellow";
-//    } else if(detectedColor.green > detectedColor.blue && detectedColor.green > detectedColor.red && redBlueDifference < 0.15) {
-//      colorGuess = "Green";
-//    } else {
-//      colorGuess = "Blue";
-//    }
-//
-//    SmartDashboard.putString("Color", colorGuess);
+    if (previousColor != colorString) {
+      if (previousColor == "Blue" && colorString == "Yellow") {
+        rotationCount = rotationCount + 1;
+        previousColor = colorString;
+      }
+      if (previousColor == "Red" && colorString == "Green") {
+        rotationCount = rotationCount + 1;
+        previousColor = colorString;
+      }
+      if (previousColor == "Green" && colorString == "Blue") {
+        rotationCount = rotationCount + 1;
+        previousColor = colorString;
+      }
+      if (previousColor == "Yellow" && colorString == "Red") {
+        rotationCount = rotationCount + 1;
+        previousColor = colorString;
+      }
+    }
+    SmartDashboard.putNumber("Rotation Count", rotationCount / 8);
     return colorString;
   }
 }
