99be4a8d8bf31d1084e99683c785fd4d4cf7a024
fuzzyfish1 <zain@accucalc.com>
2020-07-16 17:58:23 -0500

    this came from the correct branch this time

--- src/main/java/team/gif/robot/commands/autos/OppFiveBall.java
+++ src/main/java/team/gif/robot/commands/autos/OppFiveBall.java
@@ -4,12 +4,19 @@ import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.CentripetalAccelerationConstraint;
 import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.*;
-import team.gif.lib.RobotTrajectory;
+import team.gif.lib.RobotTrajectory;/*
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj2.command.*;
 import team.gif.robot.Robot;
 import team.gif.robot.commands.autoaim.Pivot;
-import team.gif.robot.commands.drivetrain.AutoDrive;
+import team.gif.robot.commands.drivetrain.AutoDrive;*/
 import team.gif.robot.commands.intake.IntakeDown;
 import team.gif.robot.commands.intake.IntakeRun;
 import team.gif.robot.commands.shooter.Fire;
@@ -19,54 +26,61 @@ import team.gif.robot.subsystems.Drivetrain;
 import java.util.List;
 
 public class OppFiveBall extends SequentialCommandGroup {
-    public Command backward6 () {
+
+
+
+    public Command drive1 () {
         Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
-            List.of(
-                new Pose2d(Units.feetToMeters(0.0), 0, new Rotation2d(0)),
-                new Pose2d(Units.feetToMeters(-6.0), 0, new Rotation2d(0))
-            ),
-            RobotTrajectory.getInstance().configReverse
+                List.of(
+                        new Pose2d(Units.feetToMeters(0.0), 0, new Rotation2d(0)),//zerod
+                        new Pose2d(Units.feetToMeters(-92/12.0), 0, new Rotation2d(0))// move backward 6ft
+                ),
+                RobotTrajectory.getInstance().configReverse
         );
         // create the command using the trajectory
         RamseteCommand rc = RobotTrajectory.getInstance().createRamseteCommand(trajectory);
         // Run path following command, then stop at the end.
         return rc.andThen(() -> Drivetrain.getInstance().tankDriveVolts(0, 0));
     }
-    public Command bigU () {
+
+    public Command drive2 (){
+        CentripetalAccelerationConstraint constraint = new CentripetalAccelerationConstraint(30);
         Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
-            List.of(
-                new Pose2d(Units.feetToMeters(0.0), 0, new Rotation2d(0)),
-                new Pose2d(Units.feetToMeters(3.0), Units.feetToMeters(-3.0), new Rotation2d(Units.degreesToRadians(-90.0))),
-                new Pose2d(Units.feetToMeters(3.0), Units.feetToMeters(-10.0), new Rotation2d(Units.degreesToRadians(-90.0))),
-                new Pose2d(Units.feetToMeters(8.0), Units.feetToMeters(-15.0), new Rotation2d(Units.degreesToRadians(0.0)))
-            ),
-            RobotTrajectory.getInstance().configForward
+                List.of(
+                        new Pose2d(Units.feetToMeters(-92/12.0), 0, new Rotation2d(0)),
+                        new Pose2d(Units.feetToMeters(-58/12.0), Units.feetToMeters(-10.0), new Rotation2d(Units.degreesToRadians(0)))
+                ),
+                RobotTrajectory.getInstance().configForward.addConstraint(constraint)// this is what was added
         );
         // create the command using the trajectory
         RamseteCommand rc = RobotTrajectory.getInstance().createRamseteCommand(trajectory);
         // Run path following command, then stop at the end.
         return rc.andThen(() -> Drivetrain.getInstance().tankDriveVolts(0, 0));
-    }
-    public OppFiveBall () {
-        System.out.println("Auto: Opponent Five Ball Selected");
 
-        // still under development, right now just drives backward and
-        // turns ~45 degrees, moving to the right 3 feet
-        addCommands(
-            new PrintCommand("Auto: Opponent Five Ball Started"),
-            //backward6(),
-            bigU()
-            //forward6()
+    }
+
+    /*public Command drive3(){
+        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+                List.of(
+                        new Pose2d(Units.feetToMeters(94.36/12), 0, new Rotation2d(0)),
+                        new Pose2d(Units.feetToMeters(4), -, new Rotation2d(-35))
+                ),
+                RobotTrajectory.getInstance().configReverse
         );
-    }
+        // create the command using the trajectory
+        RamseteCommand rc = RobotTrajectory.getInstance().createRamseteCommand(trajectory);
+        // Run path following command, then stop at the end.
+        return rc.andThen(() -> Drivetrain.getInstance().tankDriveVolts(0, 0));
+    }*/
 
-/*
     public OppFiveBall() {
-        System.out.println("Auto: Opponent 5 Ball Selected");
-        //
-        // TODO: speed up moveback
-        //
 
+
+        //System.out.println("Auto: Opponent 5 Ball Selected");
+        /*
+         * TODO: speed up moveback
+         * */
+/*
         addCommands(
                 new IntakeDown(),
                 new AutoDrive(.5,.30,.30), // slow rampup to reduce jitter when starting
@@ -85,9 +99,27 @@ public class OppFiveBall extends SequentialCommandGroup {
                                          new RevFlywheel(),
                                          new Fire(true))
 
+        );*/
+
+        addCommands(
+                new PrintCommand("Auto: Opponent 5 Ball Selected"),// init
+                new IntakeDown(),
+                new ParallelDeadlineGroup(drive1(),new IntakeRun()),//enemy ball heist
+                new IntakeRun().withTimeout(1),
+                drive2(),//get out of there
+                //drive3(), //turn and move forward
+                new ParallelCommandGroup(
+                        // let it rip
+                        new RevFlywheel(),
+                        new Fire(false))
+
         );
     }
-
- */
 }
-
+/** my dude i think i found probable issue
+ * the issue is probably that the centripital acceleration cap is to low
+ * i think this because it doesnt turn all the way to the measurement we want it to
+ * because it is looking for a larger radius not given by the waypoints or a higher tangential velocity
+ * i think that increasing the centripital acceleration cap might be a viable solution because the bot
+ * is relatively difficult to flip because it is lower to the ground
+ * **/
