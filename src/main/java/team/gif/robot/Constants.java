dcf262858bf596f106beeff8e331a5acd833ccde
BIll Knopf <billknopfjr@yahoo.com>
2020-07-21 23:10:40 -0500

    Remove unused constants in constants file

--- src/main/java/team/gif/robot/Constants.java
+++ src/main/java/team/gif/robot/Constants.java
@@ -22,7 +22,6 @@ public final class Constants {
         public static final double kF = 0.000175; // 0.000166;
         public static final double RPM_LOW = Robot.isCompBot ? 4400 : 4600; // C:4300 P: 4600 // P was 4500
         public static final double RPM_HIGH = 4550; // was 4550
-        public static final double maxVelocity = 5000;
     }
 
     public static class Hanger {
@@ -35,16 +34,8 @@ public final class Constants {
 
         public static final double GRAV_FEED_FORWARD = 300 / 1023.0; // Percent constant to counteract gravity
 
-        public static final int ALLOWABLE_ERROR = 100; // Error to allow move command to end
-        public static final int MAX_VELOCITY = 2000; // RPM
-        public static final int MIN_VELOCITY = 0;
-        public static final int MAX_ACCELERATION = 1500;
-
         public static final int MAX_POS = 160;
         public static final int MIN_POS = 1;
-
-
-        public static final int DEPLOYED_POS = 15000;
 
         // Color Wheel
         public static final int COLOR_WHEEL_POSITION = 25;
@@ -59,38 +50,12 @@ public final class Constants {
     }
 
     public static class drivetrain {
-        public static double METERS_PER_TICK_LEFT = 100;// needs a legitimate number
-        public static double METERS_PER_TICK_RIGHT = 100;// needs a legitimate number
-
-        public static double TRACK_WIDTH = 10;//inches
-
-
         public static double WHEEL_DIAMETER = 0.127; // IN METERS
         public static double WHEEL_CICUMFERENCE = WHEEL_DIAMETER * 3.14159; // IN METERS
-        public static double WHEEL_RADIUS = 0.0635;
-        public static double ENCODER_EPR = 4096; // EPR = Edges per revolution (IN TICKS)
 
         public static double TICKS_TO_METERS = 10000;
         public static double TICKS_TO_METERS_LEFT = 10115; //10058; // Pushed bot 17 feet, recorded ticks (51816), converted to meters
         public static double TICKS_TO_METERS_RIGHT = 10005; // 9915; // 9915; // Pushed bot 17 feet, recorded ticks (51816), converted to meters
-        public static double DPS_TO_MPS = ((WHEEL_DIAMETER * 0.0254 * Math.PI) / (360) * (10)); // Degrees per sec. to meters per sec.
-
-        public static double kTrackWidth = 0.76884201;
-
-        // Must be tuned
-        public static double DRIVE_P = 0.0;
-        public static double DRIVE_I = 0.0;
-        public static double DRIVE_D = 0.0;
-        public static double DRIVE_ANGLE_P = 0.0;
-        public static double DRIVE_ANGLE_I = 0.0;
-        public static double DRIVE_ANGLE_D = 0.0;
-        public static double DRIVE_ANGLE_I_ZONE = 0.0;
-        public static double DRIVE_STRAIGHT_ANGLE_P = 0.0;
-        public static double DRIVE_DIST_TOLERANCE = 0;
-        public static double DRIVE_ANGLE_TOLERANCE = 0;
-
-        public static double WheelDiameter = 0.127;
-        public static double encoderEPR;
 
         // trajectory
         // from FRC Characterization Tool
