d56a4c15ae608b1a3ae8ddb9242addebd4718db3
BIll Knopf <billknopfjr@yahoo.com>
2020-07-21 22:54:32 -0500

    SafeFiveBall Remove previous auto

--- src/main/java/team/gif/robot/commands/autos/SafeFiveBall.java
+++ src/main/java/team/gif/robot/commands/autos/SafeFiveBall.java
@@ -102,24 +102,4 @@ public class SafeFiveBall extends SequentialCommandGroup {
                         new Fire(false))
         );
     }
-
-    /*public SafeFiveBall(){
-        System.out.println("Auto: Safe Five Ball Selected");
-
-        /*
-        * TODO: speed up moveback
-        *
-
-        addCommands(
-                new IntakeDown(),
-                new ParallelDeadlineGroup(new AutoDrive(2.3,.47,.47),//OG 2.5 ----- time was 2.3
-                                          new IntakeRun()),
-                new IntakeRun().withTimeout(0.5), // sit in position to fully collect ball
-                new ParallelCommandGroup(new Pivot(),
-                                         new RevFlywheel()).withTimeout(2.5),
-                new ParallelCommandGroup(new Pivot(),
-                                         new RevFlywheel(),
-                                         new Fire(true))
-        );
-    }*/
 }
