c46bc39339d8d7690709c27553926f45be082238
BIll Knopf <billknopfjr@yahoo.com>
2020-07-21 22:38:02 -0500

    Restore Mobility and MobilityFwd

--- src/main/java/team/gif/robot/commands/autos/Mobility.java
+++ src/main/java/team/gif/robot/commands/autos/Mobility.java
@@ -14,25 +14,12 @@ import team.gif.robot.subsystems.Drivetrain;
 import java.util.List;
 
 public class Mobility extends SequentialCommandGroup {
-    public Command forward () {
-        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
-            List.of(
-                new Pose2d(Units.feetToMeters(0.0), 0, new Rotation2d(0)),
-                new Pose2d(Units.feetToMeters(20.0), 0, new Rotation2d(0))
-            ),
-            RobotTrajectory.getInstance().configForward
-        );
-        // create the command using the trajectory
-        RamseteCommand rc = RobotTrajectory.getInstance().createRamseteCommand(trajectory);
-        // Run path following command, then stop at the end.
-        return rc.andThen(() -> Drivetrain.getInstance().tankDriveVolts(0, 0));
-    }
 
     public Command reverse () {
         Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
             List.of(
-                new Pose2d(Units.feetToMeters(20.0), 0, new Rotation2d(0)),
-                new Pose2d(Units.feetToMeters(0.0), 0, new Rotation2d(0))
+                new Pose2d(Units.feetToMeters(0.0), 0, new Rotation2d(0)),
+                new Pose2d(Units.feetToMeters(-3.0), 0, new Rotation2d(0))
             ),
             RobotTrajectory.getInstance().configReverse
         );
@@ -45,13 +32,8 @@ public class Mobility extends SequentialCommandGroup {
     public Mobility() {
         System.out.println("Auto: Mobility Selected");
 
-        // still under development, Used as a test path
-        // For now, drives 15 feet forward, backward, forward, backward
         addCommands(
             new PrintCommand("Auto: Mobility Started"),
-            forward(),
-            reverse(),
-            forward(),
             reverse()
         );
     }
--- src/main/java/team/gif/robot/commands/autos/MobilityFwd.java
+++ src/main/java/team/gif/robot/commands/autos/MobilityFwd.java
@@ -1,14 +1,41 @@
 package team.gif.robot.commands.autos;
 
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import team.gif.robot.commands.drivetrain.forward;
+import team.gif.lib.RobotTrajectory;
+import team.gif.robot.subsystems.Drivetrain;
+import java.util.List;
 
 public class MobilityFwd extends SequentialCommandGroup {
 
+    public Command forward () {
+        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+            List.of(
+                new Pose2d(Units.feetToMeters(0.0), 0, new Rotation2d(0)),
+                new Pose2d(Units.feetToMeters(3.0), 0, new Rotation2d(0))
+            ),
+            RobotTrajectory.getInstance().configForward
+        );
+        // create the command using the trajectory
+        RamseteCommand rc = RobotTrajectory.getInstance().createRamseteCommand(trajectory);
+        // Run path following command, then stop at the end.
+        return rc.andThen(() -> Drivetrain.getInstance().tankDriveVolts(0, 0));
+    }
+
     public MobilityFwd() {
         System.out.println("Auto: Mobility Forward Selected");
+
         addCommands(
-                new forward(2.0,-.3,-.3)
+            new PrintCommand("Auto: Mobility Forward Started"),
+            forward()
         );
     }
+
 }
