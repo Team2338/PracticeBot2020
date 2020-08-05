50108c6669ec9f4a3e70b1ebc2abc59d588cd409
fuzzyfish1 <zain@accucalc.com>
2020-08-04 21:08:05 -0500

    angled pickup

---
+++ src/main/java/team/gif/robot/commands/autos/Opp_angled_pickup.java
@@ -0,0 +1,77 @@
+package team.gif.robot.commands.autos;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj2.command.*;
+import team.gif.lib.RobotTrajectory;
+import team.gif.robot.commands.intake.IntakeDown;
+import team.gif.robot.commands.intake.IntakeRun;
+import team.gif.robot.commands.shooter.Fire;
+import team.gif.robot.commands.shooter.RevFlywheel;
+import team.gif.robot.subsystems.Drivetrain;
+
+import java.util.List;
+
+public class Opp_angled_pickup extends SequentialCommandGroup {
+
+    //only 2 tunable #s
+    public double theta = 15;
+    public double y = -12/12;
+    public double x1df =10;// x1 distance factor
+
+    // for sake of not having to tune as much, math
+    public double x1 = 10/12;
+    public double x2 = 10/12;
+
+    // measured numbers
+    public double linetoball = 129/12;
+    public double robotlongth = 35.5/12;
+
+    public Command reverse() {
+
+        x2 = y*Math.tan(theta);
+        x1 = ((linetoball-robotlongth)-x2)+x1df;
+
+        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+            List.of(
+                new Pose2d(Units.feetToMeters(0.0), 0, new Rotation2d(0)),     //zerod
+                new Pose2d(Units.feetToMeters(x1), 0, new Rotation2d(0)), // move backward 6ft
+                new Pose2d(Units.feetToMeters(x2),y, new Rotation2d(theta))
+            ),
+            RobotTrajectory.getInstance().configReverseSlow
+        );
+        // create the command using the trajectory
+        RamseteCommand rc = RobotTrajectory.getInstance().createRamseteCommand(trajectory);
+        // Run path following command, then stop at the end.
+        return rc.andThen(() -> Drivetrain.getInstance().tankDriveVolts(0, 0));
+    }
+
+    public Command forward(){
+        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+                List.of(
+                        new Pose2d(Units.feetToMeters(x2),y, new Rotation2d(theta)),
+                        new Pose2d(Units.feetToMeters(-2), Units.feetToMeters(-14.0), new Rotation2d(Units.degreesToRadians(-7.0)))
+                ),
+                RobotTrajectory.getInstance().configForward
+        );
+        // create the command using the trajectory
+        RamseteCommand rc = RobotTrajectory.getInstance().createRamseteCommand(trajectory);
+        // Run path following command, then stop at the end.MaxVelocityConstraint
+        return rc.andThen(() -> Drivetrain.getInstance().tankDriveVolts(0, 0));
+    }
+
+    public Opp_angled_pickup() {
+        addCommands(
+                new PrintCommand("Auto: Opponent 5 Ball Selected"),
+                new IntakeDown(),
+                new ParallelDeadlineGroup(
+                    reverse(),
+                    new IntakeRun()),//enemy ball heist
+                new IntakeRun().withTimeout(.75),
+                forward()    //get out of there
+        );
+    }
+}
