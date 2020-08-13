f555e689d7951dcda8c63d7833483a3a1a224eb2
fuzzyfish1 <zain@accucalc.com>
2020-08-13 15:56:58 -0500

    added commentary double checked some math

--- src/main/java/team/gif/robot/commands/drivetrain/rotate.java
+++ src/main/java/team/gif/robot/commands/drivetrain/rotate.java
@@ -11,7 +11,7 @@ public class rotate extends CommandBase {
     public double offset = 0;
     public double kP = Constants.drivetrain.kPDriveVelLeft;
     public double kF = Constants.drivetrain.ksVolts;
-    public double margin = 1;
+    public double margin = 2;
     public double steering_adjust = 0;
     public double target = 0;
     public double pos = 0;
@@ -32,14 +32,26 @@ public class rotate extends CommandBase {
         offset = ((((target - pos)+ 540)%360)-180);
         // above math came from the math stack exchange ie stackoverflow but with nerds
         // + offset turn right by convention
-        if (offset < -margin ) {
+        if (offset < -margin/2) {
             steering_adjust = kP*offset - kF;
-        } else if (offset > margin) {
+            leftpower += steering_adjust;
+            rightpower -= steering_adjust;
+        } else if (offset > margin/2) {
             steering_adjust = kP*offset + kF;
+            leftpower += steering_adjust;
+            rightpower -= steering_adjust;
+        } else if (Math.abs(offset) <= margin/2){
+            leftpower = 0;
+            rightpower = 0;
         }
 
-        leftpower += steering_adjust;
-        rightpower -= steering_adjust;
+        // i thought that leftpower = kP
+        // in the limelight docs they use += instead
+        // this does not make sense to me because if it is not at the target but close it still acelerates towards it
+        // just not as fast and
+        // In their offset they have offset > or < 1 which does not make sense to me
+        // if that was a deadband of sorts then it should have been < -1 or > 1 making a margin of 2 degrees
+        // i just noticed that the indentations for all the files are not consistant so can we go to the standard 5 space
 
         Drivetrain.getInstance().tankDriveVolts(leftpower,rightpower);
     }
