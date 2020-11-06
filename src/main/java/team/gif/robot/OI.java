8db6ede1f908afe4a7886df3b44fd617f7572a6b
Justin <justt0023@gmail.com>
2020-11-05 21:36:24 -0600

    Created Joystick test mode

--- src/main/java/team/gif/robot/OI.java
+++ src/main/java/team/gif/robot/OI.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
 import team.gif.lib.AxisButton;
+import team.gif.robot.commands.JoystickTestMode;
 import team.gif.robot.commands.autoaim.LimelightAutoAim;
 import team.gif.robot.commands.autoaim.Pivot;
 import team.gif.robot.commands.controlpanel.Rotation;
@@ -105,6 +106,7 @@ public class OI {
         aBack.whenPressed(new ControlPanelDown());
         aA.whileHeld(new Rotation());
         aB.whenPressed(new ControlPanelDown());
+        aLS.whileHeld(new JoystickTestMode());
 
         // TODO: Get better button
         aX.whileHeld(new RapidFire(false));
---
+++ src/main/java/team/gif/robot/commands/JoystickTestMode.java
@@ -0,0 +1,48 @@
+package team.gif.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import team.gif.robot.Globals;
+import team.gif.robot.Robot;
+
+public class JoystickTestMode extends CommandBase {
+    //@SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private int loopCounter;
+
+    public JoystickTestMode() {
+        // Use addRequirements() here to declare subsystem dependencies.
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        loopCounter = 0;
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        loopCounter++;
+        System.out.println(loopCounter);
+        if (loopCounter == 250){
+            Globals.gJoystickTestMode = !Globals.gJoystickTestMode;
+            System.out.println(Globals.gJoystickTestMode);
+        }
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        System.out.println("Final State " + Globals.gJoystickTestMode);
+    }
+
+    @Override
+    public boolean runsWhenDisabled() {
+        return true;
+    }
+}
