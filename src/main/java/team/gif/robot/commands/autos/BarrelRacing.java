a5147c248b07d3b0576b65c661047994358f39de
BIll Knopf <billknopfjr@yahoo.com>
2021-02-01 19:14:37 -0600

    Gave markers a wider berth

--- src/main/java/team/gif/robot/commands/autos/BarrelRacing.java
+++ src/main/java/team/gif/robot/commands/autos/BarrelRacing.java
@@ -27,24 +27,24 @@ public class BarrelRacing extends SequentialCommandGroup {
      *      ^ Robot ^
      */
 
-    private double xInit = 2.5;
+    private double xInit = 3.0;
     private double yInit = 7.5;
 
     public Command forward () {
         Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
             List.of(
-                    new Pose2dFeet().set(2.5 - xInit, 7.5 - yInit, 0.0), // C1
+                    new Pose2dFeet().set(0.0, 0.0,0.0), // C1
                     new Pose2dFeet().set(12.5 - xInit, 7.5 - yInit, 0.0), // C5
-                    new Pose2dFeet().set(15.0 - xInit, 5.0 - yInit, 90.0), // D6
-                    new Pose2dFeet().set(12.5 - xInit, 2.5 - yInit, 180.0), // E5
-                    new Pose2dFeet().set(10.0 - xInit, 5.0 - yInit, -90.0), // D4
+                    new Pose2dFeet().set(15.5 - xInit, 5.0 - yInit, 90.0), // ~D6
+                    new Pose2dFeet().set(12.5 - xInit, 2.0 - yInit, 180.0), // ~E5
+                    new Pose2dFeet().set(9.5 - xInit, 5.0 - yInit, -90.0), // ~D4
                     new Pose2dFeet().set(12.5 - xInit, 7.5 - yInit, 0.0), // C5
                     new Pose2dFeet().set(20.0 - xInit, 7.5 - yInit, 0.0), // C8
                     new Pose2dFeet().set(23.0 - xInit, 10.0 - yInit, -90.0), // ~B9
-                    new Pose2dFeet().set(20.0 - xInit, 13.0 - yInit, 180.0), // ~A8
-                    new Pose2dFeet().set(17.5 - xInit, 10.0 - yInit, 90.0), // B7
-                    new Pose2dFeet().set(25.0 - xInit, 2.5 - yInit, 0.0), // E10
-                    new Pose2dFeet().set(27.5 - xInit, 5.0 - yInit, -90.0), // D11
+                    new Pose2dFeet().set(20.0 - xInit, 12.75 - yInit, 180.0), // ~A8
+                    new Pose2dFeet().set(17.0 - xInit, 10.0 - yInit, 90.0), // ~B7
+                    new Pose2dFeet().set(22.5 - xInit, 2.5 - yInit, 0.0), // ~E9
+                    new Pose2dFeet().set(27.75 - xInit, 5.0 - yInit, -90.0), // ~D11
                     new Pose2dFeet().set(25.0 - xInit, 7.5 - yInit, 180.0), // C10
                     new Pose2dFeet().set(2.5 - xInit, 7.5 - yInit, 180.0) // C1
 
